755dfe7193739992f397b159009158b0b391a842
Sandy Maguire <sandy@sandymaguire.me>
2023-04-29 16:45:10 -0700

    puppet man

---
+++ resources/puppets/man/export.sh
@@ -0,0 +1,17 @@
+for i in {0..15}; do magick "man.xcf[${i}]" "${i}.png"; done
+mv 0.png LeftCalf.png
+mv 1.png LeftThigh.png
+mv 2.png LeftBicep.png
+mv 3.png LeftForearm.png
+mv 4.png LeftHand.png
+mv 5.png Abdomen.png
+mv 6.png RightCalf.png
+mv 7.png RightThigh.png
+mv 8.png RightFoot.png
+mv 9.png LeftFoot.png
+mv 10.png Torso.png
+mv 11.png RightBicep.png
+mv 12.png RightForearm.png
+mv 13.png RightHand.png
+mv 14.png Head.png
+mv 15.png Sword.png
