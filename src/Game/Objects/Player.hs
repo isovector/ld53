304dd5ffa5902ea8dab4d67a5a242177f52d0bbd
Sandy Maguire <sandy@sandymaguire.me>
2023-04-28 19:00:10 -0700

    make him duck

--- src/Game/Objects/Player.hs
+++ src/Game/Objects/Player.hs
@@ -28,13 +28,17 @@ player pos0 = loopPre 0 $ proc (oi, vel) -> do
 
   let arrows = c_dir $ controls oi
 
+  let is_ducking = view _y arrows == 1
+
   let vel'0 = fmap fromIntegral arrows ^* (60 * dt)
 
   let vel' = updateVel onGround dt vel vel'0
 
+  let ore = bool playerOre duckingOre is_ducking
+
   let dpos = vel' ^* dt
   let desiredPos = pos + coerce dpos
-  let pos' = fromMaybe pos $ move collision (coerce playerOre) pos $ dpos
+  let pos' = fromMaybe pos $ move collision (coerce ore) pos $ dpos
 
   let vel''
         = (\want have res -> bool 0 res $ abs(want - have) <= epsilon )
@@ -46,7 +50,7 @@ player pos0 = loopPre 0 $ proc (oi, vel) -> do
 
   let dir = True
 
-  drawn <- drawPlayer -< (dir, pos', False)
+  drawn <- drawPlayer -< (dir, pos', ore, False)
 
   returnA -< (, vel'') $
     ObjectOutput
@@ -58,7 +62,7 @@ player pos0 = loopPre 0 $ proc (oi, vel) -> do
         , oo_state =
             oi_state oi
               & #os_pos .~ pos'
-              & #os_collision .~ Just playerOre
+              & #os_collision .~ Just ore
               & #os_tags %~ S.insert IsPlayer
               & #os_facing .~ dir
         , oo_render = drawn
@@ -67,8 +71,14 @@ player pos0 = loopPre 0 $ proc (oi, vel) -> do
 playerOre :: OriginRect Double
 playerOre = OriginRect sz $ sz & _x *~ 0.5
 
+duckingOre :: OriginRect Double
+duckingOre = OriginRect ducksz $ ducksz & _x *~ 0.5
+
 sz :: Num a => V2 a
 sz = V2 24 60
+
+ducksz :: Num a => V2 a
+ducksz = V2 24 30
 
 
 touchingGround :: (V2 WorldPos -> Bool) -> OriginRect Double -> V2 WorldPos -> Bool
@@ -127,9 +137,9 @@ dieAndRespawnHandler = proc (pos, on_die) -> do
 
 
 
-drawPlayer :: SF (Bool, V2 WorldPos, Bool) Renderable
+drawPlayer :: SF (Bool, V2 WorldPos, OriginRect Double, Bool) Renderable
 drawPlayer =
-  proc (dir, pos, is_totsugeku) -> do
+  proc (dir, pos, ore, is_totsugeku) -> do
     -- We can fully animate the player as a function of the position!
     V2 vx vy <- derivative -< pos
     r <- mkAnim
@@ -140,7 +150,7 @@ drawPlayer =
             , pos
             )
     returnA -< mconcat
-      [ drawOriginRect (V4 0 255 0 255) playerOre pos
+      [ drawOriginRect (V4 0 255 0 255) ore pos
       , r
       ]
 
