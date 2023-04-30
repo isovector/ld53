aa2862b8fb94f169f6044858c4767ee8f72b8032
Sandy Maguire <sandy@sandymaguire.me>
2023-04-30 12:34:18 -0700

    stay in your screen idiot

--- src/Engine/Camera.hs
+++ src/Engine/Camera.hs
@@ -6,6 +6,7 @@ module Engine.Camera
 
 import Engine.Types
 import FRP.Yampa
+import Engine.Utils (posToTile)
 
 
 getCameraFocus :: ObjectState -> V2 WorldPos
@@ -13,14 +14,33 @@ getCameraFocus os = os_pos os + coerce (os_camera_offset os)
 
 camera
     :: V2 WorldPos
-    -> SF (FrameInfo' a, V2 WorldPos)
+    -> SF (FrameInfo, V2 WorldPos)
           Camera
 camera = flip loopPre $ arr $ \((fi, focus), pos) -> do
   let dt = fi_dt fi
       pos' = pos + (focus - pos) ^* coerce dt * cameraSpeed
-  case focus == pos || distance focus pos <= cameraDeadzone of
-    True -> (Camera $ centerScreen pos, pos)
-    False -> (Camera $ centerScreen pos', pos')
+  let desired =
+        case focus == pos || distance focus pos <= cameraDeadzone of
+          True -> pos
+          False -> pos'
+      actual = centerScreen
+             $ keepInRect (fmap (fromIntegral . getPixel) . l_bounds . head . gs_loaded_levels $ fi_global fi)
+             $ desired
+  ( Camera actual
+    , desired
+    )
+
+keepInRect :: Rect WorldPos -> V2 WorldPos -> V2 WorldPos
+keepInRect (Rect (V2 l t) (V2 w h)) (V2 x y) =
+  let (V2 hw hh) = halfLogic in
+  V2 (clamp (l + hw, l + w - hw) x) (clamp (t + hh, t + h - hh) y)
+
+halfLogic :: V2 WorldPos
+halfLogic = logicalSize * 0.5
+
+clamp :: Ord a => (a, a) -> a -> a
+clamp (low, high) a = min high (max a low)
+
 
 
 cameraSpeed :: Num a => a
