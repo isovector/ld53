1eb55aafac061bb36ee6b192dd4ba2f52da38d99
Andrew McKnight <thedonk@gmail.com>
2023-04-29 20:12:23 -0700

    Fall and Jump Attacks

--- src/Game/Resources.hs
+++ src/Game/Resources.hs
@@ -190,8 +190,11 @@ getPuppetAnim PlayerStab        = CannedAnim ManPuppet "man" "Stab" 0.35 1000 Fa
 getPuppetAnim PlayerRun         = CannedAnim ManPuppet "man" "Run" 0.35 1000 True
 getPuppetAnim PlayerTakeoff     = CannedAnim ManPuppet "man" "Takeoff" 0.35 1000 False
 getPuppetAnim PlayerJump        = CannedAnim ManPuppet "man" "Jump" 0.35 1000 True
+getPuppetAnim PlayerJumpStab    = CannedAnim ManPuppet "man" "JumpStab" 0.35 1000 True
 getPuppetAnim PlayerFall        = CannedAnim ManPuppet "man" "Fall" 0.35 500 True
+getPuppetAnim PlayerFallSlice   = CannedAnim ManPuppet "man" "FallSlice" 0.35 500 True
 getPuppetAnim PlayerDucking     = CannedAnim ManPuppet "man" "Ducking" 0.35 1000 True
 getPuppetAnim PlayerDucked      = CannedAnim ManPuppet "man" "Ducked" 0.35 500 True
 getPuppetAnim PlayerUnducking   = CannedAnim ManPuppet "man" "Unducking" 0.35 1000 True
+getPuppetAnim PlayerDuckStab    = CannedAnim ManPuppet "man" "DuckStab" 0.35 1000 True
 
