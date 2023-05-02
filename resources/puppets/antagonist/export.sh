9643b58becc68602cdb3a4f16b470dfe616bacd0
Sandy Maguire <sandy@sandymaguire.me>
2023-05-01 17:09:08 -0700

    add new puppets

---
+++ resources/puppets/antagonist/export.sh
@@ -0,0 +1,17 @@
+for i in {0..15}; do magick "man.xcf[${i}]" "${i}.png"; done
+mv 0.png AntagonistLeftCalf.png
+mv 1.png AntagonistLeftThigh.png
+mv 2.png AntagonistLeftBicep.png
+mv 3.png AntagonistLeftForearm.png
+mv 4.png AntagonistLeftHand.png
+mv 5.png AntagonistAbdomen.png
+mv 6.png AntagonistRightCalf.png
+mv 7.png AntagonistRightThigh.png
+mv 8.png AntagonistRightFoot.png
+mv 9.png AntagonistLeftFoot.png
+mv 10.png AntagonistTorso.png
+mv 11.png AntagonistRightBicep.png
+mv 12.png AntagonistRightForearm.png
+mv 13.png AntagonistRightHand.png
+mv 14.png AntagonistHead.png
+mv 15.png AntagonistSword.png
